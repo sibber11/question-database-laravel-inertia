a2a0ad89f5f40b772bfa04b84461b1a044f4cca3
Sibber Ahmad <sibber11@gmail.com>
2024-06-16 13:00:33 +0600

    fields minimized

--- resources/js/Pages/Courses/Fields.vue
+++ resources/js/Pages/Courses/Fields.vue
@@ -39,62 +39,52 @@ function submit() {
     <Head title="Dashboard"/>
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-        </template>
+        <form class="mt-6 space-y-6" @submit.prevent="submit">
+            <div>
+                <InputLabel for="name" value="Name"/>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="p-6 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <form class="mt-6 space-y-6" @submit.prevent="submit">
-                        <div>
-                            <InputLabel for="name" value="Name"/>
+                <TextInput
+                    id="name"
+                    v-model="form.name"
+                    autocomplete="name"
+                    autofocus
+                    class="mt-1 block w-full"
+                    required
+                    type="text"
+                />
 
-                            <TextInput
-                                id="name"
-                                v-model="form.name"
-                                autocomplete="name"
-                                autofocus
-                                class="mt-1 block w-full"
-                                required
-                                type="text"
-                            />
-
-                            <InputError :message="form.errors.name" class="mt-2"/>
-                        </div>
-
-                        <div>
-                            <InputLabel for="semester_id" value="Semester"/>
-
-                            <SelectInput
-                                id="semester_id"
-                                v-model="form.semester_id"
-                                class="mt-1 block w-full"
-                                required
-                            >
-                                <option v-for="model in semesters.data" :value="model.id">
-                                    {{ model.label }}
-                                </option>
-                            </SelectInput>
-
-                            <InputError :message="form.errors.semester_id" class="mt-2"/>
-                        </div>
-
-                        <div class="flex items-center gap-4">
-                            <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
-
-                            <Transition
-                                enter-active-class="transition ease-in-out"
-                                enter-from-class="opacity-0"
-                                leave-active-class="transition ease-in-out"
-                                leave-to-class="opacity-0"
-                            >
-                                <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
-                            </Transition>
-                        </div>
-                    </form>
-                </div>
+                <InputError :message="form.errors.name" class="mt-2"/>
             </div>
-        </div>
+
+            <div>
+                <InputLabel for="semester_id" value="Semester"/>
+
+                <SelectInput
+                    id="semester_id"
+                    v-model="form.semester_id"
+                    class="mt-1 block w-full"
+                    required
+                >
+                    <option v-for="model in semesters.data" :value="model.id">
+                        {{ model.label }}
+                    </option>
+                </SelectInput>
+
+                <InputError :message="form.errors.semester_id" class="mt-2"/>
+            </div>
+
+            <div class="flex items-center gap-4">
+                <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+
+                <Transition
+                    enter-active-class="transition ease-in-out"
+                    enter-from-class="opacity-0"
+                    leave-active-class="transition ease-in-out"
+                    leave-to-class="opacity-0"
+                >
+                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                </Transition>
+            </div>
+        </form>
     </AuthenticatedLayout>
 </template>
--- resources/js/Pages/Questions/Fields.vue
+++ resources/js/Pages/Questions/Fields.vue
@@ -65,110 +65,101 @@ onMounted(function () {
     <Head title="Dashboard"/>
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-        </template>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="p-6 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <form class="mt-6 space-y-6" @submit.prevent="submit">
-                        <div>
-                            <InputLabel for="title" value="Title"/>
+        <form class="mt-6 space-y-6" @submit.prevent="submit">
+            <div>
+                <InputLabel for="title" value="Title"/>
 
-                            <TextInput
-                                id="title"
-                                v-model="form.title"
-                                class="mt-1 block w-full"
-                                required
-                                type="text"
-                            />
+                <TextInput
+                    id="title"
+                    v-model="form.title"
+                    class="mt-1 block w-full"
+                    required
+                    type="text"
+                />
 
-                            <InputError :message="form.errors.title" class="mt-2"/>
-                        </div>
-
-                        <div>
-                            <InputLabel for="semester_id" value="Semester"/>
-
-                            <SelectInput
-                                id="semester_id"
-                                v-model="form.semester_id"
-                                class="mt-1 block w-full"
-                                required
-                            >
-                                <option v-for="model in semesters.data" :value="model.id">
-                                    {{ model.label }}
-                                </option>
-                            </SelectInput>
-
-                            <InputError :message="form.errors.semester_id" class="mt-2"/>
-                        </div>
-
-                        <div>
-                            <InputLabel for="course_id" value="Course"/>
-
-                            <SelectInput
-                                id="course_id"
-                                v-model="form.course_id"
-                                class="mt-1 block w-full"
-                                required
-                            >
-                                <option v-for="model in coursesOfSemester" :value="model.id">
-                                    {{ model.label }}
-                                </option>
-                            </SelectInput>
-
-                            <InputError :message="form.errors.course_id" class="mt-2"/>
-                        </div>
-
-                        <div>
-                            <InputLabel for="chapter_id" value="Chapter"/>
-
-                            <SelectInput
-                                id="chapter_id"
-                                v-model="form.chapter_id"
-                                class="mt-1 block w-full"
-                                required
-                            >
-                                <option v-for="model in chapterOfCourse" :value="model.id">
-                                    {{ model.label }}
-                                </option>
-                            </SelectInput>
-
-                            <InputError :message="form.errors.chapter_id" class="mt-2"/>
-                        </div>
-
-                        <div>
-                            <InputLabel for="chapter_id" value="Description"/>
-
-                            <MdEditor :preview="false" language="en-US" v-model="form.description"/>
-
-                            <InputError :message="form.errors.description" class="mt-2"/>
-                        </div>
-
-                        <div>
-                            <InputLabel for="answer" value="Answer" class="mb-2"/>
-
-                            <MdEditor :preview="false" language="en-US" v-model="form.answer"/>
-
-                            <InputError :message="form.errors.answer" class="mt-2"/>
-                        </div>
-
-
-                        <div class="flex items-center gap-4">
-                            <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
-
-                            <Transition
-                                enter-active-class="transition ease-in-out"
-                                enter-from-class="opacity-0"
-                                leave-active-class="transition ease-in-out"
-                                leave-to-class="opacity-0"
-                            >
-                                <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
-                            </Transition>
-                        </div>
-                    </form>
-                </div>
+                <InputError :message="form.errors.title" class="mt-2"/>
             </div>
-        </div>
+
+            <div>
+                <InputLabel for="semester_id" value="Semester"/>
+
+                <SelectInput
+                    id="semester_id"
+                    v-model="form.semester_id"
+                    class="mt-1 block w-full"
+                    required
+                >
+                    <option v-for="model in semesters.data" :value="model.id">
+                        {{ model.label }}
+                    </option>
+                </SelectInput>
+
+                <InputError :message="form.errors.semester_id" class="mt-2"/>
+            </div>
+
+            <div>
+                <InputLabel for="course_id" value="Course"/>
+
+                <SelectInput
+                    id="course_id"
+                    v-model="form.course_id"
+                    class="mt-1 block w-full"
+                    required
+                >
+                    <option v-for="model in coursesOfSemester" :value="model.id">
+                        {{ model.label }}
+                    </option>
+                </SelectInput>
+
+                <InputError :message="form.errors.course_id" class="mt-2"/>
+            </div>
+
+            <div>
+                <InputLabel for="chapter_id" value="Chapter"/>
+
+                <SelectInput
+                    id="chapter_id"
+                    v-model="form.chapter_id"
+                    class="mt-1 block w-full"
+                    required
+                >
+                    <option v-for="model in chapterOfCourse" :value="model.id">
+                        {{ model.label }}
+                    </option>
+                </SelectInput>
+
+                <InputError :message="form.errors.chapter_id" class="mt-2"/>
+            </div>
+
+            <div>
+                <InputLabel for="chapter_id" value="Description"/>
+
+                <MdEditor :preview="false" language="en-US" v-model="form.description"/>
+
+                <InputError :message="form.errors.description" class="mt-2"/>
+            </div>
+
+            <div>
+                <InputLabel for="answer" value="Answer" class="mb-2"/>
+
+                <MdEditor :preview="false" language="en-US" v-model="form.answer"/>
+
+                <InputError :message="form.errors.answer" class="mt-2"/>
+            </div>
+
+
+            <div class="flex items-center gap-4">
+                <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
+
+                <Transition
+                    enter-active-class="transition ease-in-out"
+                    enter-from-class="opacity-0"
+                    leave-active-class="transition ease-in-out"
+                    leave-to-class="opacity-0"
+                >
+                    <p v-if="form.recentlySuccessful" class="text-sm text-gray-600">Saved.</p>
+                </Transition>
+            </div>
+        </form>
     </AuthenticatedLayout>
 </template>
